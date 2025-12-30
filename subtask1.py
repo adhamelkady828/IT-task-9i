42a9ff65c7afa0fa9a7674a8f5b4bda027e5f1bf
adhamelkady828 <s2510769030@fhooe.at>
2025-12-31 00:59:18 +0100

    Add files via upload

---
+++ subtask1.py
@@ -0,0 +1,13 @@
+def largest_square(n):
+    i = 0
+
+    while (i * i) <= n:
+        i = i + 1
+
+    q = (i - 1) * (i - 1)
+    return q
+
+
+n = 31
+q = largest_square(n)
+print("Largest square less than or equal to", n, "is", q)
